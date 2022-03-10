7045a84752e27ce4d00c3387750925f2b28990bb
mohamed ahmed abd almageed <mohamedmaim97@gmail.com>
2022-03-10 19:07:36 +0200

    add greeter function from feature B

--- fun.js
+++ fun.js
@@ -2,6 +2,11 @@ const my_function = () => {
   console.log("hello world");
 };
 
+const greeter = (name) => {
+  console.log(`Hi ${name}`);
+};
+
 module.exports = {
-  my_function
+  my_function,
+  greeter
 };
